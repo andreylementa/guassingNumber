bc055d396aca40b55f90830178c27fda9a614ff6
Andrey Lementa <andrejkudrasov79070@gmail.com>
2023-05-20 09:29:01 +0300

    create layout and click check

---
+++ script.js
@@ -0,0 +1,6 @@
+'use strict';
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guessingNumber = Number(document.querySelector('.number-input').value);
+  console.log(guessingNumber, typeof guessingNumber);
+});
